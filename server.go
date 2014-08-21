d643faa01fd72145e32d614df8ef7ab3c4be0751
Jonas Falck <jonas.falck@gmail.com>
2014-08-21 21:19:41 +0200

    get temp in go server

--- server.go
+++ server.go
@@ -1,11 +1,29 @@
 package main
 
-import "github.com/go-martini/martini"
+import (
+	"github.com/go-martini/martini"
+	"io/ioutil"
+	"net/http"
+)
 
 func main() {
 	m := martini.Classic()
 	m.Get("/", func() string {
 		return "Hello world!"
 	})
+	m.Get("/api/temp", getTemp)
 	m.Run()
+}
+
+//Download temp from temperatur.nu.
+func getTemp() []byte {
+	resp, err := http.Get("http://www.temperatur.nu/termo/soder/termo.txt")
+	defer resp.Body.Close()
+
+	if err != nil {
+		return []byte("error")
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	return body
 }
