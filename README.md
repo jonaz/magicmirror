2606a0eab2b5dd119e08820ec26417390ac77718
Jonas Falck <jonas.falck@gmail.com>
2016-06-09 23:32:57 +0200

    add deploy scripts

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-magicMirror
+magicmirror
 ===========
 
-Web gui and go backend for running my new project magicMirror. Which will be a mirror with display in the background. 
+Web gui and go backend for running my new project magicmirror. Which will be a mirror with display in the background. 
 Will use Polymer for GUI and golang for backend. Websockets for communication. 
--- dockerCrosscompile.sh
+++ dockerCrosscompile.sh
@@ -1,4 +1,4 @@
 #docker run --rm -it -v "$(pwd)":/usr/src/myapp -w /usr/src/myapp golang:1.3.1-cross bash -c ./crossBuild.sh
 
-docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp -e GOOS=linux -e GOARCH=arm golang:1.6 /bin/bash -c "go get -d -v ; go build -v -o magicMirror-arm"
+docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp -e GOOS=linux -e GOARCH=arm golang:1.6 /bin/bash -c "go get -d -v ; go build -v -o magicmirror"
 
